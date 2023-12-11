f98af119129226b1cbc8ca959b7c8fc974600e75
Koen Klinkers <k.klinkers@gmail.com>
2023-12-11 16:01:10 +0100

    finally

--- toolkit/src/grid.rs
+++ toolkit/src/grid.rs
@@ -29,9 +29,35 @@ pub fn surrounding_coor(x: usize, y: usize, x_max: usize, y_max: usize) -> Vec<(
 
     coor
 }
+
+pub fn neighbor_coor(x: usize, y: usize, x_max: usize, y_max: usize) -> Vec<(usize, usize)> {
+    let mut coor: Vec<(usize, usize)> = Vec::new();
+    if x > 0 {
+        coor.push((x - 1, y));
+    }
+    if y > 0 {
+        coor.push((x, y - 1));
+    }
+
+    if y < y_max {
+        coor.push((x, y + 1));
+    }
+
+    if x < x_max {
+        coor.push((x + 1, y));
+    }
+
+    coor
+}
+#[derive(Clone, Debug)]
+pub struct Grid<T: Clone>(pub Vec<Vec<T>>);
+
 impl<T: std::clone::Clone> Grid<T> {
-    pub fn get(&self, coor: (usize, usize)) -> &T {
-        &self.0[coor.1][coor.0]
+    pub fn get(&self, coor: (usize, usize)) -> Option<&T> {
+        if coor.0 > self.max_column() || coor.1 > self.max_row() {
+            return None;
+        }
+        Some(&self.0[coor.1][coor.0])
     }
 
     pub fn max_row(&self) -> usize {
@@ -46,13 +72,17 @@ impl<T: std::clone::Clone> Grid<T> {
         let surrounding_coor = surrounding_coor(coor.0, coor.1, self.max_column(), self.max_row());
         surrounding_coor
             .iter()
-            .map(|x| (*x, self.get(*x)))
+            .map(|x| (*x, &self.0[x.0][x.1]))
+            .collect::<Vec<_>>()
+    }
+    pub fn get_neighbors(&self, coor: (usize, usize)) -> Vec<((usize, usize), &T)> {
+        let neighbor_coor = neighbor_coor(coor.0, coor.1, self.max_column(), self.max_row());
+        neighbor_coor
+            .iter()
+            .map(|x| (*x, &self.0[x.0][x.1]))
             .collect::<Vec<_>>()
     }
 }
-
-#[derive(Clone, Debug)]
-pub struct Grid<T: std::clone::Clone>(pub Vec<Vec<T>>);
 
 pub struct GridBorrow<'a, T: std::clone::Clone> {
     grid: &'a Grid<T>,
@@ -81,37 +111,54 @@ impl<'a, T: Clone> Iterator for GridBorrow<'a, T> {
             return None;
         }
         let result = &self.grid.0[self.row][self.col];
+        let coor = (self.row, self.col);
         self.col += 1;
         if self.col > self.grid.max_column() {
             self.row += 1;
             self.col = 0;
         }
-        Some(((self.col, self.row), result))
+        Some((coor, result))
     }
 }
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[derive(Clone, PartialEq, Eq, Debug)]
+    pub struct Tile(usize);
     #[test]
     fn test_grid_iter() {
-        let grid: Grid<usize> = Grid(vec![vec![0, 1], vec![2, 3]]);
+        let grid: Grid<Tile> = Grid(vec![vec![Tile(0), Tile(1)], vec![Tile(2), Tile(3)]]);
         let items: Vec<_> = grid.into_iter().collect();
 
-        assert_eq!(*items[2].1, 2);
+        assert_eq!(items[0], ((0, 0), &Tile(0)));
+        assert_eq!(items[1], ((0, 1), &Tile(1)));
+        assert_eq!(items[2], ((1, 0), &Tile(2)));
+        assert_eq!(items[3], ((1, 1), &Tile(3)));
     }
     #[test]
     fn test_grid_get() {
         let grid: Grid<usize> = Grid(vec![vec![0, 1], vec![2, 3]]);
         let two = grid.get((0, 1));
 
-        assert_eq!(*two, 2);
+        assert_eq!(*two.unwrap(), 2);
+
+        let not_exist = grid.get((9, 9));
+        assert!(not_exist.is_none())
     }
     #[test]
     fn test_grid_get_surounding() {
-        let grid: Grid<usize> = Grid(vec![vec![0, 1], vec![2, 3]]);
-        let tiles = grid.get_surrounding((0, 1));
+        let grid: Grid<usize> = Grid(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 9]]);
+        assert_eq!(grid.get_surrounding((0, 1)).len(), 5);
+        assert_eq!(grid.get_surrounding((0, 0)).len(), 3);
+        assert_eq!(grid.get_surrounding((1, 1)).len(), 8);
+    }
 
-        assert_eq!(tiles.len(), 3);
+    #[test]
+    fn test_grid_get_neighbors() {
+        let grid: Grid<usize> = Grid(vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 9]]);
+        assert_eq!(grid.get_neighbors((0, 1)).len(), 3);
+        assert_eq!(grid.get_neighbors((0, 0)).len(), 2);
+        assert_eq!(grid.get_neighbors((1, 1)).len(), 4);
     }
 }
